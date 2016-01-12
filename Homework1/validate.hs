3e47b7011da17bd0150f0c29566c8451418e3d1d
Blankpanda <Blankpanda8@gmail.com>
2016-01-12 17:48:36 -0500

    finished exercise 3

--- Homework1/validate.hs
+++ Homework1/validate.hs
@@ -3,8 +3,13 @@ main :: IO ()
 
 toDigits :: Integer -> [Integer]
 toDigitsRev :: Integer -> [Integer]
-reverseInt :: Integer -> Integer
 doubleEveryOther :: [Integer] -> [Integer]
+sumDigits :: [Integer] -> Integer
+
+-- helpers
+reverseInt :: Integer -> Integer
+sumList :: [Integer] -> Integer
+expandDigits :: [Integer] -> [Integer]
 
 -- reverses an integers order.
 reverseInt 0 = 0
@@ -12,13 +17,23 @@ reverseInt n
   | (n <= 9)    = n
   | otherwise = read (reverse (show n)) :: Integer
 
+-- sum a list
+sumList [] = 0
+sumList (x:xs) =  x + sumList xs
+
+-- expand values of digits
+expandDigits [] = []
+expandDigits (x:xs)
+    | x < 10    = x : xs
+    | x >= 10   = (expandDigits xs) ++ (toDigits x)
+
 
 -- converts an Integer into a list of elements where each digit is an element
 -- in the the list EX: 6751 -> [6,7,5,1]
 toDigits 0 = []
 toDigits n
-  | n < 0     = []
-  | n <= 10   = n : []
+  | n <= 0     = []
+  | n < 10   = n : []
   | otherwise = toDigits (n `div` 10 ) ++ [n `mod` 10]
 
 -- converts the reverse of a Integer into a list of elements where each
@@ -34,13 +49,16 @@ doubleEveryOther []       = []
 doubleEveryOther (x:[])   = [x]
 doubleEveryOther (y:x:xs) = (y * 2) : doubleEveryOther (xs ++ (toDigits x))
 
+-- sums an expanded list
+sumDigits [] = 0
+sumDigits n = sumList ((expandDigits n))
 
 
 
 main = do
   let n = toDigits 1234
   let z = toDigitsRev 1234
-  let t = doubleEveryOther [5,1,5,1]
+  let t = sumList [10,15,5,1]
   print n
   print z
   print t
--- Notes/sum2.hs
+++ Notes/sum2.hs
@@ -7,4 +7,4 @@ doubleEveryOther :: [Integer] -> [Integer]
 -- doubles every other element in a list
 doubleEveryOther []       = []
 doubleEveryOther (x:[])   = [x]
-doubleEveryOther (y:x:xs) = (y * 2) : doubleEveryOther xs
+doubleEveryOther (y:x:xs) = (y * 2) : doubleEveryOther (xs ++ (x : []))
