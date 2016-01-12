aa0818df06bfecfc30847398a8526496b376ac39
Blankpanda <Blankpanda8@gmail.com>
2016-01-12 17:51:46 -0500

    notes

--- Notes/sum2.hs
+++ Notes/sum2.hs
@@ -1,3 +1,5 @@
+sumList :: [Integer] -> Integer
+
 sumEveryTwo :: [Integer] -> [Integer]
 sumEveryTwo []         = []
 sumEveryTwo (x:[])     = [x]
@@ -7,4 +9,4 @@ doubleEveryOther :: [Integer] -> [Integer]
 -- doubles every other element in a list
 doubleEveryOther []       = []
 doubleEveryOther (x:[])   = [x]
-doubleEveryOther (y:x:xs) = (y * 2) : doubleEveryOther (xs ++ (x : []))
+doubleEveryOther (y:x:xs) = (y * 2) : doubleEveryOther
