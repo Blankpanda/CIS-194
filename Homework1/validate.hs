6bd27a3295f38a2360338c7e980e4724073f66a4
Blankpanda <Blankpanda8@gmail.com>
2016-01-14 09:00:50 -0500

    finished exercise 4, fixed exercise 3

--- Homework1/validate.hs
+++ Homework1/validate.hs
@@ -5,7 +5,7 @@ toDigits :: Integer -> [Integer]
 toDigitsRev :: Integer -> [Integer]
 doubleEveryOther :: [Integer] -> [Integer]
 sumDigits :: [Integer] -> Integer
-validate :: Integer -> Integer
+validate :: Integer -> Bool
 
 -- helpers
 reverseInt :: Integer -> Integer
@@ -17,7 +17,7 @@ expandDigits :: [Integer] -> [Integer]
 reverseInt 0 = 0
 reverseInt n
   | (n <= 9)    = n
-  | otherwise = read (reverse (show n)) :: Integer
+  | otherwise   = read (reverse (show n)) :: Integer
 
 -- sum a list
 sumList [] = 0
@@ -26,8 +26,8 @@ sumList (x:xs) =  x + sumList xs
 -- expand values of digits
 expandDigits [] = []
 expandDigits (x:xs)
-    | x < 10    = x : xs
-    | x >= 10   = (expandDigits xs) ++ (toDigits x)
+    | x < 10    = x :  expandDigits xs
+    | x >= 10   = (toDigits x) ++ (expandDigits xs)
 
 
 -- converts an Integer into a list of elements where each digit is an element
@@ -35,8 +35,8 @@ expandDigits (x:xs)
 toDigits 0 = []
 toDigits n
   | n <= 0     = []
-  | n < 10   = n : []
-  | otherwise = toDigits (n `div` 10 ) ++ [n `mod` 10]
+  | n < 10     = n : []
+  | otherwise  = toDigits (n `div` 10 ) ++ [n `mod` 10]
 
 -- converts the reverse of a Integer into a list of elements where each
 -- digit is an element in the list EX: 6751 -> [1,5,7,6]
@@ -53,13 +53,16 @@ doubleEveryOther (y:x:xs) = (2*y):x:(doubleEveryOther xs)
 
 -- sums an expanded list
 sumDigits [] = 0
-sumDigits n = sumList ((expandDigits n))
+sumDigits n  = sumList ((expandDigits n))
 
 -- validates credit card
+-- (sumDigits ( doubleEveryOther ( toDigits n )) `mod` 10
 validate n
-  | (sumDigits ( doubleEveryOther ( toDigits n )) `mod` 10 == 0 = True
-  | otherwise                                                   = False
+  | sumDigits ( doubleEveryOther ( toDigits n) ) `mod` 10 == 0 = True
+  | otherwise                                                  = False
 
 main = do
-  let t = validate 4012888888881881
+  let x = sumDigits ( doubleEveryOther (toDigits 4012888888881882)) `mod` 10
+  let t = validate 4012888888881882
+  print x
   print t
